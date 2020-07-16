60ce00b050a2b8639f924648d039975adc1480ff
Daniel Fernandez <39488892+dferndz@users.noreply.github.com>
2020-07-15 20:05:16 -0500

    firebase support

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+// Firebase config
+import { firebaseConfig } from "../firebase.config";
+import * as firebase from "firebase/app";
+require("firebase/analytics");
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 const App = () => <p>Hello world</p>;
 
 export default App;
